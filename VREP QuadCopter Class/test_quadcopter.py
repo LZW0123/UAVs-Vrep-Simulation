498f1c8501093f0cb04861e1cea8ad18df932443
LZW0123 <1589598459@qq.com>
2022-06-11 21:53:01 +0800

    fix code for env

--- VREP QuadCopter Class/test_quadcopter.py
+++ VREP QuadCopter Class/test_quadcopter.py
@@ -10,7 +10,7 @@ q = Quadcopter()
 for i in range(1000):
 
 	# prints the position, the same can be confirmed from inside VREP
-	print q.get_position()
+	print (q.get_position())
 
 	time.sleep(0.1)
 
